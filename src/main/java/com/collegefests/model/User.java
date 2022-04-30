5fc9bff65b3c8de83a1901a19d126ceeee993267
gjaidev <jaidevgilla1995@gmail.com>
2022-04-29 19:13:02 -0500

    Added auto increment for userId

--- src/main/java/com/collegefests/model/User.java
+++ src/main/java/com/collegefests/model/User.java
@@ -4,9 +4,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.io.Serializable;
 
 @Entity
@@ -16,6 +14,7 @@ import java.io.Serializable;
 @Setter
 public class User implements Serializable {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int userId;
     private String userName;
     private String password;
